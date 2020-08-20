f6d6b09d37b646fd24991d338b6eaf50bb2f8ea1
Azeem <azeemahmed101@gmail.com>
2020-08-20 09:54:49 +0200

    add contact function

--- src/contact.js
+++ src/contact.js
@@ -1,5 +1,19 @@
-const contact = () => {
+const contact = (() => {
   const content = document.getElementById('contact');
-};
+
+  const location = document.createElement('h2');
+  const phoneNum = document.createElement('h2');
+  const openHours = document.createElement('h2');
+
+  location.innerHTML = 'Location<p>2 Lexington Avenue, New York, NY 10010</p>';
+
+  phoneNum.innerHTML = 'Call us<p>212.777.2410</p>';
+
+  openHours.innerHTML = 'Open Hours<p>Mon-Fri: 11AM-11pm</p>';
+
+  content.appendChild(location);
+  content.appendChild(phoneNum);
+  content.appendChild(openHours);
+})();
 
 export default contact;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import homePage from './home.js';
 import menu from './menu.js';
+import contact from './contact.js';
 console.log('Hello');
