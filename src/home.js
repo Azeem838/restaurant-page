8c4d778c7c1a862047351b6cc157367243c47784
Azeem <azeemahmed101@gmail.com>
2020-08-20 10:41:50 +0200

    fix linter errors

--- src/home.js
+++ src/home.js
@@ -2,12 +2,10 @@ const homePage = () => {
   const content = document.getElementById('content');
 
   const subHeading = document.createElement('h3');
-  subHeading.innerHTML =
-    'Chef Rob Hanzer perfectly cooks chickens in a giant rotisserie oven, but the birds are equally delicious roasted on a rack in a home oven.';
+  subHeading.innerHTML = 'Chef Rob Hanzer perfectly cooks chickens in a giant rotisserie oven, but the birds are equally delicious roasted on a rack in a home oven.';
 
   const desc = document.createElement('p');
-  desc.innerHTML =
-    'Located in the Gramercy Park Hotel, Maialino captures the warmth and comfort of a Roman trattoria, with a menu that reflects the rich cultures of both Rome and New York. <br /> Executive Chef Matt Spivey draws inspiration from the Roman cooking tradition to create soulful food that celebrates the restaurant’s ongoing relationship with its local family of farmers and suppliers.';
+  desc.innerHTML = 'Located in the Gramercy Park Hotel, Maialino captures the warmth and comfort of a Roman trattoria, with a menu that reflects the rich cultures of both Rome and New York. <br /> Executive Chef Matt Spivey draws inspiration from the Roman cooking tradition to create soulful food that celebrates the restaurant’s ongoing relationship with its local family of farmers and suppliers.';
 
   const image = document.createElement('img');
   image.setAttribute('src', './roast-chicken.jpg');
